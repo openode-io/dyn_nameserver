f7697009752830555f27bcf4277ea13bca108ec5
Martin Levesque <levesque.martin@gmail.com>
2019-04-07 09:31:15 -0400

    ps all

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 bash build.sh
 
-docker rm -f $(docker ps | grep dyn-nameserver | awk '{print $1}')
+docker rm -f $(docker ps -a | grep dyn-nameserver | awk '{print $1}')
 
 docker run -d --restart=always -p 53:53 -p 53:53/udp --name dyn-nameserver --net="host" --env-file=.env  -v $(pwd):/opt/app dyn-nameserver bash build-and-run.sh
 
